91d5dea4a9cb49e1d752c560ea40e27fe8d1877d
Akshit-Kumar098 <akshit098kumar@gmail.com>
2024-11-15 11:50:51 +0530

    onclickbutton

--- src/App.js
+++ src/App.js
@@ -10,15 +10,20 @@ function App() {
       <Student/>
       <Useme/>
     </div>
+    
   );
+  
 }
 
 function Student(){
+ 
   return(
    <div>
      <h1>Akshit</h1>
      <h2>Rahul</h2>
      <h3>Vishal</h3>
+     <button onClick={btn}>click me</button>
+     
    </div>
     
   )
@@ -33,11 +38,19 @@ class Useme extends Component{
           <h1>Currently persuing btech from dpgitm</h1>
           <h1>Currently persuing btech from sgt</h1>
           <h1>Currently persuing btech from dsd</h1>
+          <button onClick={()=>alert("hey")}>click on me </button>
           </div>
       )
   }
 
 }
+function btn(){
+  return(
+    alert("hello")
+  )
+}
+
+
 
 
 export default App;

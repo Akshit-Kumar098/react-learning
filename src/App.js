a83a7ccf4524d375f451168fc683d5f7a15d8841
Akshit-Kumar098 <akshit098kumar@gmail.com>
2024-11-15 11:03:33 +0530

    rect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      <Student/>
     </div>
   );
+}
+
+function Student(){
+  return(
+   <div>
+     <h1>Akshit</h1>
+     <h2>Rahul</h2>
+     <h3>Vishal</h3>
+   </div>
+    
+  )
 }
 
 export default App;

027bed7fd891f93b307978c8504bbf24b3d737d3
Akshit-Kumar098 <akshit098kumar@gmail.com>
2024-11-18 10:00:26 +0530

    useeffect

--- src/Useeffect.js
+++ src/Useeffect.js
@@ -1,13 +1,19 @@
 import React,{useEffect,useState} from "react";
 function Red(){
-    const[count,setCount]=useState(0)
+    const[count,setCount]=useState(100)
+    const[data,setData]=useState(10)
     useEffect(()=>{
         console.log("useeffect")
-    })
+    },[data])
+    useEffect(()=>{
+        console.log("useeffect")
+    },[count])
     return(
         <div>
-            <h1>useeffect in react {count}</h1>
+            <h1>count: {count}</h1>
+            <h1>data: {data}</h1>
             <button onClick={()=>setCount(count+1)}>tap tap</button>
+            <button onClick={()=>setData(data+1)}> tap</button>
         </div>
     )
 }

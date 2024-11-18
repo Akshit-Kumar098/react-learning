79b4475a4634f9c21f1427fe3be887f6618ccfff
Akshit-Kumar098 <akshit098kumar@gmail.com>
2024-11-18 11:05:15 +0530

    bootstrap

--- src/Headings.js
+++ src/Headings.js
@@ -1,12 +1,17 @@
 import React from "react";
 import './style.css'
 import style from './custom.module.css'
+import Button from 'react-bootstrap/Button';
 function Design(){
     return(
         <div>
             <h1 className="primary">Hello gyzzzzzz</h1>
             <h1 style={{color:'pink'}}>Hello gyzzzzzz</h1>
             <h1 className={style.success}>Hello gyzzzzzz</h1>
+            <Button className="button" variant="primary">Primary</Button>
+      <Button className="button" variant="secondary">Secondary</Button>
+      <Button className="button" variant="success">Success</Button>
+
         </div>
     )
 }
